0f08a0a7fe5bda3dfa37afebde01ddceb44931d9
Andrei Sava <asava@sorenson.com>
2025-02-24 15:28:10 +0200

    make the Demo work

---
+++ Demo SwiftUI/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  Demo SwiftUI
+//
+//  Created by Narcis Badea on 06.05.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+                .foregroundColor(Color(UIColor.label))
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Demo SwiftUI/Demo_SwiftUIApp.swift
@@ -0,0 +1,39 @@
+import SwiftUI
+import AppnomixCommerce
+
+@main
+struct Demo_SwiftUIApp: App {
+    
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+    @State private var onboardingEvent: OnboardingEvent? = .unknown
+    
+    var body: some Scene {
+        WindowGroup {
+            if onboardingEvent == .onboardingDropout || onboardingEvent == .onboardingCompleted {
+                ContentView()
+            } else {
+                AppnomixOnboardingView(onboardingEvent: $onboardingEvent)
+            }
+        }
+    }
+}
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil
+    ) -> Bool {
+        AppnomixCommerceSDK.start(
+            clientID: "YOUR_CLIENT_ID", // ask Appnomix
+            authToken: "YOUR_AUTH_TOKEN", // ask Appnomix
+            appGroupName: "group.app.appnomix.demo-swiftui", // e.g., group.com.mycompany.myapp
+            appURLScheme: "appnomix-demo-swiftui://", // e.g., my-app-url://
+            requestLocation: true,
+            requestTracking: true
+        )
+        AnalyticsFacade().trackOfferDisplay(context: "app_start")
+        
+        return true
+    }
+}
+
